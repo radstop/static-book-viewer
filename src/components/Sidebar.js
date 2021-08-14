4cfcd7ee4284da2f7623eba665607c92916c1809
Masoud <masoudtabatabaei20@gmail.com>
2021-08-14 22:29:57 +0430

    fetch inventory in Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,41 @@
+function fetchInventory() {
+    let result = [];
+    let lessonId = 0;
+
+    let res = fetch('../../course-list-dom.html').then(function (response){
+        return response.text();
+    }).then(function (text) {
+        let parser = new DOMParser();
+        const doc3 = parser.parseFromString(text, "text/html");
+        let courseSection = doc3.querySelectorAll(".course-section");
+
+        courseSection.forEach(course => {
+            let session = {};
+            let lessonsArr = [];
+            let lessonlinks = course.querySelectorAll(".section-list .lesson-link");
+            
+            session["session"] = course.querySelector(".section-title").textContent;
+            lessonlinks.forEach(lessonLink => {
+                // lessonsArr.push(lessonLink.textContent.replace(/\r?\n|\r/g, ""));
+                lessonsArr.push({
+                    id : ++lessonId ,
+                    title : lessonLink.textContent.replace(/\r?\n|\r/g, "") 
+                });
+            });
+
+            session["lessons"] = lessonsArr;
+            result.push(session);
+        });
+
+        return result;
+    });
+
+    const printAddress = async () => {
+        const a = await res;
+        console.log(a);
+      };
+
+    return printAddress();
+}
+
+export default fetchInventory;
