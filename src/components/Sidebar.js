3ba4cbb29c4ddcc3c295611be97e8d24a2928eb2
Amir Salehi <nik.amirsa@gmail.com>
2021-08-15 13:29:40 +0430

    get data with fetch api in async saturation

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,75 +1,9 @@
-function fetchInventory() {
-  let result = [];
-  let lessonId = 0;
+async function fetchInventory() {
+  const response = await fetch("../../inventorySample.json");
 
-  let res = fetch("../../course-list-dom.html")
-    .then(function (response) {
-      return response.text();
-    })
-    .then(function (text) {
-      let parser = new DOMParser();
-      const doc3 = parser.parseFromString(text, "text/html");
-      let courseSection = doc3.querySelectorAll(".course-section");
+  let data = await response.json();
 
-      courseSection.forEach((course) => {
-        let session = {};
-        let lessonsArr = [];
-        let lessonlinks = course.querySelectorAll(".section-list .lesson-link");
-
-        session["session"] = course.querySelector(".section-title").textContent;
-        lessonlinks.forEach((lessonLink) => {
-          // lessonsArr.push(lessonLink.textContent.replace(/\r?\n|\r/g, ""));
-          lessonsArr.push({
-            id: ++lessonId,
-            title: lessonLink.textContent.replace(/\r?\n|\r/g, ""),
-          });
-        });
-
-        session["lessons"] = lessonsArr;
-        result.push(session);
-      });
-
-      console.log(result);
-
-      return result;
-    });
-
-  //   const printAddress = async () => {
-  //     const a = await res;
-  //     console.log(a);
-  //   };
-
-  //   fetch("../../course-list-dom.html")
-  //     .then((res) => res.text())
-  //     .then((text) => {
-  //       const parser = new DOMParser();
-  //       const doc3 = parser.parseFromString(text, "text/html");
-  //       const courseSection = doc3.querySelectorAll(".course-section");
-
-  //       courseSection.forEach((course) => {
-  //         let session = {};
-  //         let lessonsArr = [];
-  //         let lessonlinks = course.querySelectorAll(".section-list .lesson-link");
-
-  //         session["session"] = course.querySelector(".section-title").textContent;
-  //         lessonlinks.forEach((lessonLink) => {
-  //           // lessonsArr.push(lessonLink.textContent.replace(/\r?\n|\r/g, ""));
-  //           lessonsArr.push({
-  //             id: ++lessonId,
-  //             title: lessonLink.textContent.replace(/\r?\n|\r/g, ""),
-  //           });
-  //         });
-
-  //         session["lessons"] = lessonsArr;
-  //         result.push(session);
-  //       });
-
-  //       console.log(result);
-
-  //       return result;
-  //     });
-
-  //   return printAddress();
+  return data;
 }
 
 export default fetchInventory;
