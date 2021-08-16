dbe5a823c41eda261bf01ab1a07247e22b8be2e6
Masoud <masoudtabatabaei20@gmail.com>
2021-08-16 18:11:38 +0430

    implement primitive collapse-in/out sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ async function fetchInventory() {
 
   let data = await response.json();
 
-  let sidebarTemplate = '<div class="menu">';
+  let sidebarTemplate = `<div class="menu__toggle-btn">open/close</div><div class="menu">`;
   data.forEach(chapter => {
     let lessons = chapter.lessons;
     sidebarTemplate += `<div class="menu__item">
