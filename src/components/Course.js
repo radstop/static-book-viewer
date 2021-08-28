5c66a2119db11fc23040ed254bd0c89c5cafaef6
Masoud <masoudtabatabaei20@gmail.com>
2021-08-28 17:37:38 +0430

    show lesson title in browser tab title

--- src/components/Course.js
+++ src/components/Course.js
@@ -2,7 +2,9 @@ async function getCourse(lessonID) {
     let response = await fetch("../src/pages/"+ lessonID +".html");
     let body = await response.text();
 
-    return body;
+    let pageTitle = body.match("(?<=<h2>)(.*?)(?=</h2>)")[0];
+
+    return [pageTitle , body];
 }
 
 export default getCourse;
