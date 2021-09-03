f398fec3acb4fd4613a3d91a34e7a169241a245a
Amir <nik.amirsa@gmail.com>
2021-09-03 14:04:52 +0430

    close other opened menu items when another item selected

--- src/helpers/handleSidebar.js
+++ src/helpers/handleSidebar.js
@@ -1,17 +1,30 @@
-window.addEventListener('load', function () {
-  // control hide/show of sidebar items
-  document.querySelectorAll(".menu__item button").forEach(function (button) {
-    button.addEventListener("click", function (e) {
-      e.preventDefault();
-      button.parentNode.classList.toggle("menu__item--open");
-    });
-  });
+window.addEventListener(
+  "load",
+  function () {
+    // control hide/show of sidebar items
+    document.querySelectorAll(".menu__item button").forEach(function (button) {
+      button.addEventListener("click", function (e) {
+        e.preventDefault();
+        button.parentNode.classList.toggle("menu__item--open");
 
-  // control collaps-in and collapse-out sidebar menu
-  document.querySelector(".menu__toggle-btn").addEventListener("click" , function (e) {
-    e.preventDefault();
-    this.nextElementSibling.classList.toggle("menu__collapse");
-    document.querySelector(".container").classList.toggle("container__with_sb");
-  });
-  
-}, false);
+        document.querySelectorAll(".menu__item--open").forEach(function (item) {
+          if (item != button.parentNode) {
+            item.classList.remove("menu__item--open");
+          }
+        });
+      });
+    });
+
+    // control collaps-in and collapse-out sidebar menu
+    document
+      .querySelector(".menu__toggle-btn")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("menu__collapse");
+        document
+          .querySelector(".container")
+          .classList.toggle("container__with_sb");
+      });
+  },
+  false
+);
