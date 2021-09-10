361d120c58bca862dc2f44da982e866898d6c699
Amir <nik.amirsa@gmail.com>
2021-09-10 17:51:35 +0430

    improve menu icon and effect

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,10 +7,10 @@ async function fetchInventory() {
 
   <div class="menu">
   <span class="menu__toggle-btn">
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
-    style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
-    <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
-    </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+  viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);
+  transform: ;msFilter:;"><path d="M10 15h10v2H10zm-6 4h16v2H4zm6-8h10v2H10zm0-4h10v2H10zM4 3h16v2H4zm0 5v8l4-4z"></path>
+  </svg>
   </span>
   `;
 
