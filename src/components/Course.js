e6eb4685739d88525cb67b30cdf340e2c9682614
Amir Salehi <nik.amirsa@gmail.com>
2021-09-20 10:07:13 +0430

    improve code in course component

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,19 +1,18 @@
 async function getCourse(lessonID) {
-    let response = await fetch("../src/pages/"+ lessonID +".html");
-    if (!response.ok) {
-        let notFoundTemplate = `
+  let response = await fetch("../src/pages/" + lessonID + ".html");
+
+  if (!response.ok) {
+    let notFoundTemplate = `
             <div class="nf-container">
                 <img src="../../public/img/404.png">
             </div>
         `;
-        return ["404" , notFoundTemplate];
-    } else {
-        let body = await response.text();
+    return ["404", notFoundTemplate];
+  }
 
-        let pageTitle = body.match("(?<=<h2>)(.*?)(?=</h2>)")[0];
-
-        return [pageTitle , body];
-    }
+  const body = await response.text();
+  const pageTitle = body.match("(?<=<h2>)(.*?)(?=</h2>)")[0];
+  return [pageTitle, body];
 }
 
 export default getCourse;
