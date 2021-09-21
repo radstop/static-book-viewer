004d462a32f26123e2698c313f5830f801bb37a8
Amir Salehi <nik.amirsa@gmail.com>
2021-09-21 12:02:06 +0430

    improve to open and show selected item in sidebar #3

--- src/helpers/handleCourse.js
+++ src/helpers/handleCourse.js
@@ -9,30 +9,18 @@ import getCourse from "../components/Course.js";
 import setPlaceholder from "../components/Placeholder.js";
 
 export default async function getLessonContent(lessonID) {
-    const [clickedPageTitle, mainContent] = await getCourse(lessonID);
-    document.title = clickedPageTitle;
-    // show placeholder 
-    setPlaceholder();
+  const [clickedPageTitle, mainContent] = await getCourse(lessonID);
+  document.title = clickedPageTitle;
+  // show placeholder
+  setPlaceholder();
 
-    document.querySelector(".container .navigation").innerHTML = Navigation(lessonID);
-    if (clickedPageTitle !== "404") {
-        // set active to selected lesson
-        document.querySelectorAll(".submenu--active").forEach((item) => {
-            item.classList.remove("submenu--active");
-        });
-        const selectedItem = document.querySelector(`[data-link='${lessonID}']`);
-        selectedItem.parentNode.className = "submenu--active";
+  document.querySelector(".container .navigation").innerHTML =
+    Navigation(lessonID);
 
-        // open submenu after page load
-        if (!document.querySelector(".menu__item--open")) {
-            document.querySelector(".submenu--active").parentNode.parentNode.classList.add("menu__item--open");
-        }
-    }
-
-    // load main content after 1500ms to show effect of placeholder
-    setTimeout(() => {
-        document.querySelector(".container .loading").innerHTML = "";
-        document.querySelector(".container .post").innerHTML = mainContent;
-        window.Prism.highlightAll();
-    }, 1500);
+  // load main content after 1500ms to show effect of placeholder
+  setTimeout(() => {
+    document.querySelector(".container .loading").innerHTML = "";
+    document.querySelector(".container .post").innerHTML = mainContent;
+    window.Prism.highlightAll();
+  }, 0);
 }
--- src/helpers/hashChange.js
+++ src/helpers/hashChange.js
@@ -1,10 +1,33 @@
 import getLessonContent from "../helpers/handleCourse.js";
 
 window.addEventListener("hashchange", () => {
-  getLessonContent(Number(window.location.hash.replace("#", "")));
+  const lessonID = Number(window.location.hash.replace("#", ""));
+
+  getLessonContent(lessonID);
 
   window.scroll({
     top: 0,
     behavior: "smooth",
   });
+
+  const selectedItem = document.querySelector(`[data-link='${lessonID}']`);
+
+  if (selectedItem) {
+    // set active to selected lesson
+    document.querySelectorAll(".submenu--active").forEach((item) => {
+      item.classList.remove("submenu--active");
+    });
+    document.querySelectorAll(".menu__item--open").forEach((item) => {
+      item.classList.remove("menu__item--open");
+    });
+
+    selectedItem.parentNode.className = "submenu--active";
+
+    // open submenu after page load
+    if (!document.querySelector(".menu__item--open")) {
+      document
+        .querySelector(".submenu--active")
+        .parentNode.parentNode.classList.add("menu__item--open");
+    }
+  }
 });
