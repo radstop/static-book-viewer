21bf6420dd7007f3d266635cba8cf9f1282e5fa0
Amir <nik.amirsa@gmail.com>
2021-10-28 12:44:45 +0330

    remove extra codes

--- src/App.js
+++ src/App.js
@@ -5,25 +5,15 @@ import Search, { searchActions } from "./components/Search.js";
 import "./components/GoTop.js";
 import "./vendor/all.js";
 
-(async () => {
-  // put inventory content in sidebar
+const currentHash = Number(window.location.hash.replace("#", ""));
 
+(async () => {
   document.querySelector("body").innerHTML +=
     await Sidebar() + await Setting() + await Search();
 
   settingActions()
   searchActions()
 
-  const currentHash = Number(window.location.hash.replace("#", ""));
   Course(currentHash);
-
-
-
-
-
-
 })();
-
-// Search()
-
 
